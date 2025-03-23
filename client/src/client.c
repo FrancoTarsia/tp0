20327430a359b4a85d26a9e3d56e41f78a7215c8
--global <--global>
2025-03-23 16:46:34 +0000

    Creamos logger

--- client/src/client.c
+++ client/src/client.c
@@ -15,6 +15,9 @@ int main(void)
 	/* ---------------- LOGGING ---------------- */
 
 	logger = iniciar_logger();
+	log_info(logger, "Hola! Soy un log");
+	
+	// log_error(logger, "ERROR"); LOG DE ERROR CREADO POR NOSOTROS COMO PRUEBA
 
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
@@ -54,7 +57,7 @@ int main(void)
 
 t_log* iniciar_logger(void)
 {
-	t_log* nuevo_logger;
+	t_log* nuevo_logger = log_create ("tp0.log", "tp0", true, LOG_LEVEL_INFO);
 
 	return nuevo_logger;
 }
@@ -95,6 +98,7 @@ void paquete(int conexion)
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
+	log_destroy(logger);
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
 }
