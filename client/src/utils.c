03b5892ba13e1a3567d65cc32eee7845c40674b0
FrancoTarsia <ftarsia@frba.utn.edu.ar>
2025-03-31 11:45:38 -0300

    Update utils.c

--- client/src/utils.c
+++ client/src/utils.c
@@ -18,6 +18,7 @@ void* serializar_paquete(t_paquete* paquete, int bytes)
 
 int crear_conexion(char *ip, char* puerto)
 {
+	
 	struct addrinfo hints;
 	struct addrinfo *server_info;
 
@@ -29,9 +30,14 @@ int crear_conexion(char *ip, char* puerto)
 	getaddrinfo(ip, puerto, &hints, &server_info);
 
 	// Ahora vamos a crear el socket.
-	int socket_cliente = 0;
+	//int socket_cliente = 0;
+	
+	//int fd_conexion = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
+
+	int socket_cliente = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
 
 	// Ahora que tenemos el socket, vamos a conectarlo
+	connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen);
 
 
 	freeaddrinfo(server_info);
